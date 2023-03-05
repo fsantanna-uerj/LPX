2fd07c4dfdfa79e1a52665c9a4283839db6a4e2c
Francisco Sant'Anna <francisco.santanna@gmail.com>
2023-03-05 08:38:49 -0300

    ...

---
+++ 01-iter/ex-1.2.1.py
@@ -0,0 +1,34 @@
+def pre (a):
+    ret = []
+    for v in a:
+        if type(v) is str:
+            ret = ret + [v]
+        else:
+            ret = ret + pre(v)
+    return ret
+
+class Arvore:
+    def __init__ (self, a):
+        self.vec = pre(a)
+    def __iter__ (self):
+        self.i = 0
+        return self
+    def __next__ (self):
+        if self.i >= len(self.vec):
+            raise StopIteration
+        v = self.vec[self.i]
+        self.i += 1
+        return v
+
+a = [
+    "aaa",
+    [
+        "yyy",
+        [ "111", "222", "333" ],
+        "zzz"
+    ],
+    "bbb"
+]
+
+for v in Arvore(a):
+    print(v)
